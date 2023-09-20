0dd51ee7f1b9317b5ea021bda6aff84736c1dbae
dinosoupy <anishbasoup@gmail.com>
2023-09-20 19:02:40 +1000

    data preprocessing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='reorder-prediction',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Model classes',
+    author='Anish Basu',
+    license='MIT',
+)
